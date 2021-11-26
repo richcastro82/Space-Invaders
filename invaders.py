74b1d4143679d749d678ca02a7acc1805aa20867
richcastro82 <rich.castro@achesongroup.com>
2021-11-26 07:13:40 -0500

    getting started

--- invaders.py
+++ invaders.py
@@ -18,14 +18,36 @@ clock=pygame.time.Clock()
 SCREEN=pygame.display.set_mode(SIZE)
 
 
-def main():
-    clock.tick(fps)
-    pygame.display.update()
+
+def home_screen():
+    HOME_SCREEN_BG=pygame.image.load('')
+    START_BUTTON=
+    QUIT_BUTTON=
+    start_game=False
+    while (start_game==False):
+        
+    # 3. quit method
+    # 4. start game method
+    # 5. loop it all
+
+def play_game():
     SCREEN.fill((100,100,100))
     while True:
         for event in pygame.event.get():
             if event.type==pygame.QUIT:
                 pygame.quit()
+    # 1. calling elements
+    # 2. updating game screen
+    # 3. game rules
+
+
+def main():
+    clock.tick(fps)
+    pygame.display.update()
+    home_screen()
+    play_game()
+
+
 
 
 
